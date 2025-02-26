ce451c307d8664244d17b96d8a6a3536e1e94740
nSpireMJ <mkol9510@gmail.com>
2025-02-26 13:07:52 +0100

    RigidBody.py

--- RigidBody.py
+++ RigidBody.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d 
-#import Axes3D
+#import Axes3D from matplotlib
 
 def transform_point(R, o, plocal):
     return o + R.dot(plocal)
@@ -12,8 +12,8 @@ def plot_frame(ax, R, o, label, length=0.5):
     z_axis = o + R.dot(np.array([0, 0, length]))
 
     ax.quiver(o[0], o[1], o[2], x_axis[0]-o[0], x_axis[1] - o[1], x_axis[2] - o[2], color = 'r', arrow_length_ratio = 0.1)
-    ax.quiver(o[0], o[1], o[2], y_axis[0]-o[0], y_axis[1] - o[1], y_axis[2] - o[2], color = 'r', arrow_length_ratio = 0.1)
-    ax.quiver(o[0], o[1], o[2], z_axis[0]-o[0], z_axis[1] - o[1], z_axis[2] - o[2], color = 'r', arrow_length_ratio = 0.1)
+    ax.quiver(o[0], o[1], o[2], y_axis[0]-o[0], y_axis[1] - o[1], y_axis[2] - o[2], color = 'g', arrow_length_ratio = 0.1)
+    ax.quiver(o[0], o[1], o[2], z_axis[0]-o[0], z_axis[1] - o[1], z_axis[2] - o[2], color = 'b', arrow_length_ratio = 0.1)
 
     ax.text(o[0], o[1], o[2], label, fontsize = 12, color = 'k')
 
